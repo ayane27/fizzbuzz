5b8334f675b88f73bb6ee21d877b98394120efc1
Ayane Sok <ayane.sok@mrjaffesclass.com>
2023-10-30 11:40:54 -0700

    for

---
+++ fizzbuzz.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class fizzbuzz here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class fizzbuzz
+{ 
+    public static void main(String args[])
+    {
+        for (int i = 0; i <= 100; i++) {
+        System.out.println(i);
+    }
+}
+}
